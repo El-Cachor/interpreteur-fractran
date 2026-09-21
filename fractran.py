dda7b92a2d4747aa7acd6a6a27a37079fd9459ba
Camille Lecacheur <clecacheur@centrale-marseille.fr>
2026-09-21 15:12:31 +0200

    Add Facteur factorization model and tests

--- fractran.py
+++ fractran.py
@@ -1,3 +1,6 @@
+from logging import raiseExceptions
+
+
 class Fraction:
     def __init__(self, numérateur, dénominateur):
         self.numérateur = numérateur
@@ -8,3 +11,12 @@ class Fraction:
 
     def valeur(self, n):
         return self.numérateur * n // self.dénominateur
+
+class Facteur:
+    def __init__(self, facteurs):
+        self.facteurs = facteurs
+    def nombre(self, exposants):
+        a=0
+        for k in range(len(exposants)):
+            a *= self.facteurs[k] ** exposants[k]
+        return a
--- test_fractran.py
+++ test_fractran.py
@@ -1,4 +1,4 @@
-from fractran import Fraction
+from fractran import Facteur, Fraction
 
 
 def test_Fraction_init():
@@ -10,6 +10,18 @@ def test_Fraction_est_entier():
     assert Fraction(1, 2).est_entier(2)
     assert not Fraction(1, 3).est_entier(2)
 
-
 def test_Fraction_valeur():
     assert Fraction(3, 2).valeur(4) == 3 * (4 // 2)
+
+def test_Facteur_init():
+    assert Facteur([2, 3, 7]).facteurs == [2, 3, 7]
+
+
+def test_Facteur_nombre():
+    assert Facteur([2, 3, 7]).nombre([1, 2]) == (2 ** 1) * (3 ** 2)
+    assert Facteur([2, 3, 7]).nombre([1, 2, 3]) == (2 ** 1) * (3 ** 2) * (7 ** 3)
+
+
+def test_Facteur_décomposition():
+    assert Facteur([2, 3, 7]).décomposition(1) == [0, 0, 0]
+    assert Facteur([2, 3, 7]).décomposition((2**3) * (3**2) * (7)) == [3, 2, 1]
